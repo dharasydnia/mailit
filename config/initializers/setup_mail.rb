4e55702c1e122b5e422f6e1d101cdbe74fffbc3f
dharasydnia <djoshi@sydnia.com>
2012-05-22 17:34:26 +0530

    Changes in setup mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-#ActionMailer::Base.default_url_options[:host] = "http://quiet-winter-6219.herokuapp.com/"
+#ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "http://quiet-winter-6219.herokuapp.com/"
 
 Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
